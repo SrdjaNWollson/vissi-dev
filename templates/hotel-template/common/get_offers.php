877ba86535b927f8f0be5f5c0f2fb33d620f13fb
SrdjaNWollson <srdjan.nezic@wollson.rs>
2018-11-06 16:43:25 +0100

    conflict prevention

--- templates/hotel-template/common/get_offers.php
+++ templates/hotel-template/common/get_offers.php
@@ -29,11 +29,15 @@ if(isset($db) && $db !== false){
 
     $offer_id = 0;
 
-    $result_offer_file = $db->prepare('SELECT * FROM pm_offer_file WHERE id_item = :offer_id AND checked = 1 AND lang = '.LANG_ID.' AND type = \'image\' AND file != \'\' ORDER BY rank LIMIT 1');
+    $result_offer_file = $db->prepare('SELECT * FROM pm_offer_file WHERE id_item = :offer_id AND checked = 1 AND lang = '.LANG_ID.' AND type = \'image\' AND file != \'\' ORDER BY rank');
     $result_offer_file->bindParam(':offer_id', $offer_id);
 
     $result_rate = $db->prepare('SELECT MIN(price) as price FROM pm_rate WHERE id_offer = :id_offer');
     $result_rate->bindParam(':id_offer', $offer_id);
+
+    $offer_pos = "left-side";
+
+    $current = 0;
 
     foreach($result_offer as $i => $row){
                                 
@@ -46,30 +50,33 @@ if(isset($db) && $db !== false){
         $offer_alias = DOCBASE.$page_alias.'/'.text_format($offer_alias);
         
         $html .= '
-        <article class="col-sm-4 isotopeItem" itemscope itemtype="http://schema.org/LodgingBusiness">
+        <article>
             <div class="isotopeInner">
                 <a itemprop="url" href="'.$offer_alias.'">';
                     
                     if($result_offer_file->execute() !== false && $db->last_row_count() > 0){
-                        $row = $result_offer_file->fetch(PDO::FETCH_ASSOC);
-                        
-                        $file_id = $row['id'];
-                        $filename = $row['file'];
-                        $label = $row['label'];
-                        
-                        $realpath = SYSBASE.'medias/offer/medium/'.$file_id.'/'.$filename;
-                        $thumbpath = DOCBASE.'medias/offer/medium/'.$file_id.'/'.$filename;
-                        $zoompath = DOCBASE.'medias/offer/big/'.$file_id.'/'.$filename;
-                        
-                        if(is_file($realpath)){
-                            $html .= '
-                            <figure class="more-link img-container md">
-                                <img alt="'.$label.'" src="'.$thumbpath.'">
-                                <span class="more-action">
-                                    <span class="more-icon"><i class="fa fa-link"></i></span>
-                                </span>
-                            </figure>';
+                        $html .= "<div class='rooms-slider'>";
+                        foreach($result_offer_file->fetchAll(PDO::FETCH_ASSOC) as $row){
+                            //var_dump('test');
+                            $file_id = $row['id']; 
+                            $filename = $row['file'];
+                            $label = $row['label'];
+                            
+                            $realpath = SYSBASE.'medias/offer/medium/'.$file_id.'/'.$filename;
+                            $thumbpath = DOCBASE.'medias/offer/medium/'.$file_id.'/'.$filename;
+                            $zoompath = DOCBASE.'medias/offer/big/'.$file_id.'/'.$filename;
+                            
+                            if(is_file($realpath)){
+                                $html .= '
+                                <figure class="more-link img-container md">
+                                    <img alt="'.$label.'" src="'.$thumbpath.'">
+                                    <span class="more-action">
+                                        <span class="more-icon"><i class="fa fa-link"></i></span>
+                                    </span>
+                                </figure>';
+                            }
                         }
+                        $html .= "</div>";
                     }
                     $html .= '
                     <div class="isotopeContent">
@@ -93,7 +100,7 @@ if(isset($db) && $db !== false){
                                 <div class="text-muted">'.$texts['PRICE'].' / '.$texts['NIGHT'].'</div>
                             </div>
                             <div class="col-xs-6">
-                                <span class="btn btn-primary mt5 pull-right">'.$texts['MORE_DETAILS'].'</span>
+                                <span class="btn btn-green">'.$texts['BOOK_NOW'].'</span>
                             </div>
                         </div>
                     </div>
