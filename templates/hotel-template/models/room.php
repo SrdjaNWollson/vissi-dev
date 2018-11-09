8df8716d164ec2be87d5ca06837bf4b530acfa8c
alex-wollson <abatin@wollson.rs>
2018-11-09 10:47:35 +0100

    single room

--- templates/hotel-template/models/room.php
+++ templates/hotel-template/models/room.php
@@ -49,13 +49,12 @@ require(getFromTemplate('common/header.php', false)); ?>
     
     <div id="content">
         
-        <div class="container">
+        <h2 class="sec-title">
+            <?php echo $room['title']; ?>
+        </h2>
 
-            <h2 class="sec-title">
-                <?php echo $room['title']; ?>
-            </h2>
-
-            <div class="imgSlider" data-rtl="<?php echo (RTL_DIR) ? 'true' : 'false'; ?>">
+        <div class="imgSlider" data-rtl="<?php echo (RTL_DIR) ? 'true' : 'false'; ?>">
+            <div class="container">
                 <div class="imgSlider__wrapper">
                 <?php
                 $result_room_file = $db->query('SELECT * FROM pm_room_file WHERE id_item = '.$room_id.' AND checked = 1 AND lang = '.DEFAULT_LANG.' AND type = \'image\' AND file != \'\' ORDER BY rank');
@@ -78,84 +77,87 @@ require(getFromTemplate('common/header.php', false)); ?>
                 } ?>
                 </div>
             </div>
-            <div class="single-body">
-            <aside class="single-sidebar">
-                <ul class="single__details">
-                    <li><i class="icon-moon"></i><span class="cen-vertical">Minimum Stay:</span><span class="num">1</span></li>
-                    <li><i class="icon-person"></i><span class="cen-vertical">Minimum Persons:</span><span class="num">1</span></li>
-                    <li><i class="icon-price-tag"></i><span class="cen-vertical">Price From:</span><span class="num">199</span></li>
-                </ul>
-                <div class="active-period">Active for the period: <span class="period">15 Sep- 31 Mar</span></div>
-                <div class="info">
-                    <ul>
-                        <li>Information and reservation:</li>
-                        <li>office@vissidarte.com</li>
-                        <li>+382 00 000 0000</li>
+        </div>
+        <div class="single-body">
+            <div class="container">
+                <aside class="single-sidebar">
+                    <ul class="single__details">
+                        <li><i class="icon-moon"></i><span class="cen-vertical">Minimum Stay:</span><span class="num">1</span></li>
+                        <li><i class="icon-person"></i><span class="cen-vertical">Minimum Persons:</span><span class="num">1</span></li>
+                        <li><i class="icon-price-tag"></i><span class="cen-vertical">Price From:</span><span class="num">199</span></li>
                     </ul>
-                </div>
-            </aside>
+                    <div class="active-period">Active for the period: <span class="period">15 Sep- 31 Mar</span></div>
+                    <div class="info">
+                        <ul>
+                            <li>Information and reservation:</li>
+                            <li>office@vissidarte.com</li>
+                            <li>+382 00 000 0000</li>
+                        </ul>
+                    </div>
+                </aside>
 
-            <div class="single-main">
-               
-                    <?php
-                    echo $room['descr'];
-                    
-                    $short_text = strtrunc(strip_tags($room['descr']), 100);
-                    $site_url = getUrl(); ?>
+                <div class="single-main">
                    
-                    <div id="twitter" data-url="<?php echo $site_url; ?>" data-text="<?php echo $short_text; ?>" data-title="Tweet"></div>
-                    <div id="facebook" data-url="<?php echo $site_url; ?>" data-text="<?php echo $short_text; ?>" data-title="Like"></div>
-                    <div id="googleplus" data-url="<?php echo $site_url; ?>" data-curl="<?php echo DOCBASE.'js/plugins/sharrre/sharrre.php'; ?>" data-text="<?php echo $short_text; ?>" data-title="+1"></div>
-             
-                    <h5>The package includes:</h5>
-
-                    <ul class="items-3">
-                    <?php
-                    $result_rating = $db->query('SELECT count(*) as count_rating, AVG(rating) as avg_rating FROM pm_comment WHERE item_type = \'room\' AND id_item = '.$room_id.' AND checked = 1 AND rating > 0 AND rating <= 5');
-                    if($result_rating !== false && $db->last_row_count() > 0){
-                        $row = $result_rating->fetch();
-                        $room_rating = $row['avg_rating'];
-                        $count_rating = $row['count_rating'];
+                        <?php
+                        echo $room['descr'];
                         
-                        if($room_rating > 0 && $room_rating <= 5){ ?>
-                        
-                            <input type="hidden" class="rating pull-left" value="<?php echo $room_rating; ?>" data-rtl="<?php echo (RTL_DIR) ? true : false; ?>" data-size="xs" readonly="true" data-default-caption="<?php echo $count_rating.' '.$texts['RATINGS']; ?>" data-show-caption="true">
-                            <?php
-                        }
-                    } ?>
+                        $short_text = strtrunc(strip_tags($room['descr']), 100);
+                        $site_url = getUrl(); ?>
+                       
+                        <div id="twitter" data-url="<?php echo $site_url; ?>" data-text="<?php echo $short_text; ?>" data-title="Tweet"></div>
+                        <div id="facebook" data-url="<?php echo $site_url; ?>" data-text="<?php echo $short_text; ?>" data-title="Like"></div>
+                        <div id="googleplus" data-url="<?php echo $site_url; ?>" data-curl="<?php echo DOCBASE.'js/plugins/sharrre/sharrre.php'; ?>" data-text="<?php echo $short_text; ?>" data-title="+1"></div>
+                 
+                        <h5>The package includes:</h5>
 
-                    <?php
-                    $result_facility = $db->query('SELECT * FROM pm_facility WHERE lang = '.LANG_ID.' AND id IN('.$room['facilities'].') ORDER BY id',PDO::FETCH_ASSOC);
-                    if($result_facility !== false && $db->last_row_count() > 0){
-                        foreach($result_facility as $i => $row){
-                            $facility_id 	= $row['id'];
-                            $facility_name  = $row['name'];
+                        <ul class="items-3">
+                        <?php
+                        $result_rating = $db->query('SELECT count(*) as count_rating, AVG(rating) as avg_rating FROM pm_comment WHERE item_type = \'room\' AND id_item = '.$room_id.' AND checked = 1 AND rating > 0 AND rating <= 5');
+                        if($result_rating !== false && $db->last_row_count() > 0){
+                            $row = $result_rating->fetch();
+                            $room_rating = $row['avg_rating'];
+                            $count_rating = $row['count_rating'];
                             
-                            $result_facility_file = $db->query('SELECT * FROM pm_facility_file WHERE id_item = '.$facility_id.' AND checked = 1 AND lang = '.DEFAULT_LANG.' AND type = \'image\' AND file != \'\' ORDER BY rank LIMIT 1',PDO::FETCH_ASSOC);
-                            if($result_facility_file !== false && $db->last_row_count() > 0){
-                                $row = $result_facility_file->fetch();
+                            if($room_rating > 0 && $room_rating <= 5){ ?>
+                            
+                                <input type="hidden" class="rating pull-left" value="<?php echo $room_rating; ?>" data-rtl="<?php echo (RTL_DIR) ? true : false; ?>" data-size="xs" readonly="true" data-default-caption="<?php echo $count_rating.' '.$texts['RATINGS']; ?>" data-show-caption="true">
+                                <?php
+                            }
+                        } ?>
+
+                        <?php
+                        $result_facility = $db->query('SELECT * FROM pm_facility WHERE lang = '.LANG_ID.' AND id IN('.$room['facilities'].') ORDER BY id',PDO::FETCH_ASSOC);
+                        if($result_facility !== false && $db->last_row_count() > 0){
+                            foreach($result_facility as $i => $row){
+                                $facility_id 	= $row['id'];
+                                $facility_name  = $row['name'];
                                 
-                                $file_id 	= $row['id'];
-                                $filename 	= $row['file'];
-                                $label	 	= $row['label'];
-                                
-                                $realpath	= SYSBASE.'medias/facility/big/'.$file_id.'/'.$filename;
-                                $thumbpath	= DOCBASE.'medias/facility/big/'.$file_id.'/'.$filename;
+                                $result_facility_file = $db->query('SELECT * FROM pm_facility_file WHERE id_item = '.$facility_id.' AND checked = 1 AND lang = '.DEFAULT_LANG.' AND type = \'image\' AND file != \'\' ORDER BY rank LIMIT 1',PDO::FETCH_ASSOC);
+                                if($result_facility_file !== false && $db->last_row_count() > 0){
+                                    $row = $result_facility_file->fetch();
                                     
-                                if(is_file($realpath)){ ?>
-                                    <li class="facility-icon">
-                                        <img alt="<?php echo $facility_name; ?>" title="<?php echo $facility_name; ?>" src="<?php echo $thumbpath; ?>" class="tips">
-                                    </li>
-                                    <?php
+                                    $file_id 	= $row['id'];
+                                    $filename 	= $row['file'];
+                                    $label	 	= $row['label'];
+                                    
+                                    $realpath	= SYSBASE.'medias/facility/big/'.$file_id.'/'.$filename;
+                                    $thumbpath	= DOCBASE.'medias/facility/big/'.$file_id.'/'.$filename;
+                                        
+                                    if(is_file($realpath)){ ?>
+                                        <li class="facility-icon">
+                                            <img alt="<?php echo $facility_name; ?>" title="<?php echo $facility_name; ?>" src="<?php echo $thumbpath; ?>" class="tips">
+                                        </li>
+                                        <?php
+                                    }
                                 }
                             }
-                        }
-                    } ?>
-                    </ul>
-                    <a href="#dummy" class="back-link"><i class="icon-left-arrow"></i><span>Back to offers</span></a>
-                    <a href="#dummy" class="btn btn-green"><span>Book now</span></a>
-                </div>
-            </div>  
-        </div>
+                        } ?>
+                        </ul>
+                        <a href="#dummy" class="back-link"><i class="icon-left-arrow"></i><span>Back to offers</span></a>
+                        <a href="#dummy" class="btn btn-green"><span>Book now</span></a>
+                    </div>
+                </div> 
+            </div>
+        </div> 
     </div>
 </main>
