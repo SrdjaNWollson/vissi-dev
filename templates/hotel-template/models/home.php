6eedc62865a775e1c898f7a5fade47af13fa4cc3
alex-wollson <abatin@wollson.rs>
2018-11-20 16:01:48 +0100

    delay slower

--- templates/hotel-template/models/home.php
+++ templates/hotel-template/models/home.php
@@ -99,7 +99,7 @@ if($result_slide !== false){
     <!-- rooms -->
     <section id="rooms" class="prim-section">
         <div class="container">
-            <h4 class="sec-subtitle wow fadeInUp" data-wow-duration="1s" data-wow-delay="1s">a new vision of comfort</h4>
+            <h4 class="sec-subtitle wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.5s">a new vision of comfort</h4>
             <h2 class="sec-title wow fadeInUp" data-wow-duration="1s" data-wow-delay="1s">Our Rooms</h2>
         </div>
         <!-- rooms slider -->
@@ -112,7 +112,7 @@ if($result_slide !== false){
     <!-- offers --> 
     <section id="offers" class="sec-section">
         <div class="container">
-            <h4 class="sec-subtitle wow fadeInUp" data-wow-duration="1s" data-wow-delay="1s">taste of mediterranean</h4>
+            <h4 class="sec-subtitle wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.5s">taste of mediterranean</h4>
             <h2 class="sec-title wow fadeInUp" data-wow-duration="1s" data-wow-delay="1s"s>Special Offers</h2>
         </div>
         <div class="content-slider-wrapper">
